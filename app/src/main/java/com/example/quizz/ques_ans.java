7a464ad893438f9cbfe291aa57beac8642e6a81c
vishalyadav-77 <vishalyadav15044@gmail.com>
2024-09-12 16:33:28 +0530

    Updated ques_ans arrays, fixed xml files.

--- app/src/main/java/com/example/quizz/ques_ans.java
+++ app/src/main/java/com/example/quizz/ques_ans.java
@@ -2,16 +2,25 @@ package com.example.quizz;
 
 public class ques_ans{
    public static String[] questions={
-            "who is chutya?",
-            "Strongest character?"
+            "What is the capital of France?",
+           "What does CPU stand for?",
+           "Which of the following is an operating system?",
+           "What does HTML stand for?",
+           "Which language is used to style web pages?",
     };
     public static String[][] answers={
-            {"bheem","chutki","raju","all of the above"},
-            {"Gojo","Tanjiro","Goku","Naruto"}
+            {"Berlin","Paris","Rome","None"},
+            {"Central Processing Unit", "Central Program Unit", "Computer Processing Unit", "Central Process Unit"},
+            {"Microsoft Word", "Google Chrome", "Windows 10", "Adobe Photoshop"},
+            {"HyperText Markup Language", "HyperTransfer Markup Language", "HighText Markup Language", "HyperText Machine Language"},
+            {"HTML", "JavaScript", "CSS", "SQL"},
     };
     public static String[] correctans={
-            "all of the above",
-            "Goku"
+            "Paris",
+            "Central Processing Unit",
+            "Windows 10",
+            "HyperText Markup Language",
+            "CSS",
     };
 
 }
